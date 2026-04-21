7bfad0b5f2ec7c2ef12329d733d0c4f2bfd0b023
vtu26541-collab <vtu26541@veltech.edu.in>
2026-04-21 22:46:42 +0530

    Time: 1 ms (74.63%), Space: 54 MB (91.41%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,22 @@
+var isHappy = function(n) {
+
+    function getNext(num) {
+        let sum = 0;
+        while (num > 0) {
+            let digit = num % 10;
+            sum += digit * digit;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    }
+
+    let slow = n;
+    let fast = getNext(n);
+
+    while (fast !== 1 && slow !== fast) {
+        slow = getNext(slow);
+        fast = getNext(getNext(fast));
+    }
+
+    return fast === 1;
+};
